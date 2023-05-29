88b4c5217cfda7f8849e6101bb72d224d2e39197
Sheraz360 <sheraz.s.khan92@gmail.com>
2023-05-29 15:40:46 +0100

    update questions

--- starter/index.js
+++ starter/index.js
@@ -1,25 +1,79 @@
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./generateMarkdown");
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
 
-// array of questions for user
+// Array of questions for user
 const questions = [
     {
         type: 'input',
         name: 'title',
-        message: "What is the title of your project?",
+        message: 'What is the title of your project?',
       },
-];
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information:',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'Please choose a license for your project:',
+        choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-3-Clause', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please provide contribution guidelines:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide test instructions:',
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+    ];
 
-// function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log(`${fileName} has been generated successfully!`);
+  });
 }
 
-// function to initialize program
+// Function to initialize program
 function init() {
-
+  inquirer
+    .prompt(questions)
+    .then((userInput) => {
+      const markdownContent = generateMarkdown(userInput);
+      const fileName = path.join(__dirname, 'README.md');
+      writeToFile(fileName, markdownContent);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
-// function call to initialize program
+// Function call to initialize program
 init();
