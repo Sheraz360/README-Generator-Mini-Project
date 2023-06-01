bd47407bb71f023e338da6e55feca75d3a0c4e3c
Sheraz360 <sheraz.s.khan92@gmail.com>
2023-06-01 15:00:07 +0100

    debug markdownGenerate.js and generate a README through the terminal

--- starter/README.md
+++ starter/README.md
@@ -1,1 +1,33 @@
-# README Generator
+
+# Anything
+
+## Description
+Any description
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Any instructions
+
+## Usage
+Any usage information
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+Any guidlines
+
+## Tests
+Any tests
+
+## Questions
+For any questions or inquiries, please reach out via the following:
+- GitHub: [AnYtHiNg](https://github.com/AnYtHiNg)
+- Email: Any@thing.com
--- starter/generateMarkdown.js
+++ starter/generateMarkdown.js
@@ -1,7 +1,39 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data);
+  return `
+# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions or inquiries, please reach out via the following:
+- GitHub: [${data.username}](https://github.com/${data.username})
+- Email: ${data.email}
 `;
 }
 
